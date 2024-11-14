c09689fcf4e3ca67e438d77debc3f649420720a0
neatsopheng <nsopheng13@gmail.com>
2024-11-14 22:50:15 +0700

    update home content

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -12,7 +12,7 @@ const Home = () => {
             Hi, I'm <span className="text-orange-500">Sopheng</span>
           </h3>
           <p className="text-lg font-medium md:text-3xl lg:text-4xl animate-bounce">
-            Web Developer
+            Newbie Developer
           </p>
         </div>
       </div>
@@ -20,7 +20,7 @@ const Home = () => {
       <div className="flex md:flex-row flex-col items-center justify-evenly gap-10 ">
         <div className="w-full md:w-50% border-2 border-orange-500 rounded-3xl bg-[#2d2d2d] text-white py-10 px-5">
           <p className="text-md md:text-2xl font-titillium-web " data-aos="fade-in">
-          I have been passionate about the field of Web Development since I began my studies 
+          I have been passionate about the field of Software Development since I began my studies 
           in Computer Science. I am skilled in building front-end interfaces and integrating APIs, 
           as well as creating basic back-end functionality to ensure websites and web applications 
           operate smoothly. I hope to have the opportunity to work with you and contribute to 
@@ -32,50 +32,6 @@ const Home = () => {
         </div>
       </div>
     </section>
-    // <section id="home" className="h-screen flex flex-col mt-16 md:mt-24">
-    //   <h2
-    //     id="welcome"
-    //     className=" text-center font-fraunces font-bold text-2xl md:text-4xl transition duration-500 text-slate-400 animate-pulse z-[-1]"
-    //   >
-    //     Welcome to my Profile
-    //   </h2>
-    //   <div className="home-section w-full  p-5 md:p-16 lg:px-32 lg:py-24 md:flex md:items-center md:justify-center ">
-    //     <div className="home-left md:flex-1 md:flex md:flex-col md:justify-center gap-10">
-    //       <div className="main-heading">
-    //         <h1 className="text-2xl md:text-4xl lg:text-5xl font-semibold">
-    //           Hi, I'm Sopheng
-    //         </h1>
-    //         <h3 className="text-lg font-medium md:text-3xl lg:text-4xl">
-    //           Frontend Developer
-    //         </h3>
-    //       </div>
-    //       <div className="hidden md:flex md:flex-1 border-2 rounded-3xl bg-slate-700 py-10 px-10 ">
-    //         <p className="text-2xl font-extralight">
-    //           I am passionate in Web Development field since i started my major
-    //           in Computer Science. I am able to build front end with integrated
-    //           function and able to work with API and more. I hope for having a
-    //           chance to work with you
-    //         </p>
-    //       </div>
-    //     </div>
-    //     <div className="flex flex-1 flex-col gap-10 items-center justify-between pt-10 md:flex">
-    //       <div className="flex flex-1 justify-end w-80 h-80 md:w-96 md:h-96 lg:w-[26rem] lg:h-[26rem]   ">
-    //         <img
-    //           src="src\assets\9-min.PNG"
-    //           alt="pfp"
-    //           className="w-80 h-80 md:w-96 md:h-96 lg:w-[26rem] lg:h-[26rem]  object-cover    animate-none"
-    //         />
-    //       </div>
-    //       <div className="flex-1 md:hidden border-none rounded-3xl bg-slate-700 py-10 px-10">
-    //         {/* <p className="block font-titillium-web ">
-    //         I am passionate in Web Development field since i started my major in Computer
-    //           Science. I am able to build front end with integrated function and able to work
-    //           with API and more. I hope for having a chance to work with you
-    //         </p> */}
-    //       </div>
-    //     </div>
-    //   </div>
-    // </section>
   );
 };
 
